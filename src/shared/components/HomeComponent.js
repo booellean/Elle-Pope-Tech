215fc14fe4f42d57b92d874242d4c49fb817bb01
booellean <booelle.commissions@gmail.com>
2019-08-11 12:05:29 -0700

    feat: Finalized home header information and placement

--- src/shared/components/HomeComponent.js
+++ src/shared/components/HomeComponent.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Language from './SnippetLanguageComponent';
-import Contributor from './SnippetContribComponent';
-import Commit from './SnippetCommitComponent';
 import temp from '../temp.gif';
 
 class Home extends Component {
@@ -27,23 +24,6 @@ class Home extends Component {
     }
   }
 
-  createRepoNode = (item) =>{
-    return(
-      <li key={item.id}>
-        <a href={item['html_ulr']} target="_blank"><h3>{item['name']}</h3></a>
-        <p>{item['description']}</p>
-        <details>
-          <p>Size: {item['size']}</p>
-          <p>Created: {item['created_at']}</p>
-          <p>Last Commit: {item['updated_at']}</p>
-          <Language repo={item} addToState={this.addToState.bind(this)} name='total_languages' title='Languages' url={item['languages_url']}/>
-          <Commit repo={item} addToState={this.addToState.bind(this)} name='total_commits' title='Commits' url={item['commits_url'].split('{')[0]}/>
-          <Contributor repo={item} addToState={this.addToState.bind(this)} name='total_contributors' title='Contributors' url={item['contributors_url']}/>
-        </details>
-      </li>
-    );
-  }
-
   render(){
 
    if(!this.state.info){
@@ -55,13 +35,18 @@ class Home extends Component {
       </React.Fragment>
      );
    }else{
+    let options = { month: "long", day: "numeric", year: "numeric", };
+
       return(
         <React.Fragment>
           <header id='page-header'>
-            <img src={this.state.info.avatar_url} alt="I professional picture of Elle Pope a.k.a. booellean"/>
-            <h2><a href={this.state.info.html_url} target="_blank">{this.state.info.login}</a></h2>
+          <h2><a href={this.state.info.html_url} target="_blank">{this.state.info.login}</a></h2>
+            <img id= "profile-avatar" src={this.state.info.avatar_url} alt="I professional picture of Elle Pope a.k.a. booellean"/>
+            <dl>
+              <dt>Active Since : </dt><dd>{new Date(this.state.info.created_at).toLocaleDateString('en-US', options)}</dd>
+              <dt>Repos to Date: </dt><dd>{this.state.info.public_repos + this.state.info.total_private_repos}</dd>
+            </dl>
             <p>{this.state.info.bio}</p>
-            <p>Repos : {this.state.info.public_repos + this.state.info.total_private_repos}</p>
           </header>
           <article id="content">
             {this.state.bio}
