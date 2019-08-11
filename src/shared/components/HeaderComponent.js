4be62f6c9e18e951dc604d88c9b8449d502a5a91
booellean <booelle.commissions@gmail.com>
2019-08-11 16:53:02 -0700

    feat: header component styles and behaviors complete

--- src/shared/components/HeaderComponent.js
+++ src/shared/components/HeaderComponent.js
@@ -6,26 +6,58 @@ export default class Header extends Component {
   constructor(props){
     super(props);
     this.state = {
-      navOpen: false
-    };
+      open: false,
+      displayWidth: null,
+      isMobile: false
+    }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  updateWindowDimensions() {
+    this.setState({ displayWidth: window.innerWidth });
+    if(this.state.displayWidth <= 720){
+      this.setState({ isMobile : true});
+    }else{
+      this.setState({ open : false, isMobile : false});
+    }
+  }
+
+  handleClick(){
+    this.setState({open:!this.state.open});
   }
 
   render(){
+
     return(
       <React.Fragment>
         <header id='header'>
           <img src={logo} alt="Initials of Elle Pope transposed into a butterfly" id="logo"></img>
           <h1>Elle Pope</h1>
-          <nav id="site-nav">
-            <ul>
+          {/* Hamburger Menu */}
+          <div id="hamburger" className={this.state.open ? 'clickEvent' : null} onClick={()=> {this.handleClick()}}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <nav id="site-nav" className={this.state.open ? 'open' : null}>
+            <ul className={this.state.isMobile ? 'small-screen' : 'wide-screen'}>
               <li>
-                <NavLink className='nav-link' to='/'> About</NavLink>
+                <NavLink className='nav-link' to='/' onClick={()=> {this.handleClick()}}> About</NavLink>
               </li>
               <li>
-                <NavLink className='nav-link' to='/repos'> Personal</NavLink>
+                <NavLink className='nav-link' to='/repos' onClick={()=> {this.handleClick()}}> Personal</NavLink>
               </li>
               <li>
-                <NavLink className='nav-link' to='/open-source'>Open Source</NavLink>
+                <NavLink className='nav-link' to='/open-source' onClick={()=> {this.handleClick()}}>Open Source</NavLink>
               </li>
             </ul>
           </nav>
